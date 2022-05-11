be2f88dd54bacb66d9b5a4746560174782f8b2a0
Franzi Graml <franzi.graml@gmx.de>
2022-05-11 12:27:36 +0200

    Add component to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css';
+import Posts from '../src/Components/Posts';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<Posts>
+			<Component {...pageProps} />
+		</Posts>
+	);
 }
 
 export default MyApp;
