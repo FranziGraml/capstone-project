3273fd07694103e7be95242c718b260af48b0564
Franzi Graml <franzi.graml@gmx.de>
2022-05-11 11:50:21 +0200

    Add Posts data

---
+++ src/Components/Posts.js
@@ -0,0 +1,13 @@
+export default function Posts() {
+	return (
+		<div>
+			<div>
+				<h5>Username</h5>
+				<p>posts</p>
+				<h5>Kontakt</h5>
+				<p>Mail</p>
+				<p>Mobile-Phone</p>
+			</div>
+		</div>
+	);
+}
