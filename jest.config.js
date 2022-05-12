b497119c35a36f45d1f73b77705df32eb3cfed02
Franzi Graml <franzi.graml@gmx.de>
2022-05-12 14:16:15 +0200

    Add testing

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/* module.exports = {
+	setupFilesAfterEnv: ['./src/setupTests.js'],
+}; */
+
+const nextJest = require('next/jest');
+const createJestConfig = nextJest({
+	dir: './',
+});
+const customJestConfig = {
+	moduleDirectories: ['node_modules', '<rootDir>/'],
+	testEnvironment: 'jest-environment-jsdom',
+};
+module.exports = createJestConfig(customJestConfig);
