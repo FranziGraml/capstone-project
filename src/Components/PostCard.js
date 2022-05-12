4d61449bc8e897f686d260567aa3b4dc5695139c
Franzi Graml <franzi.graml@gmx.de>
2022-05-12 15:00:06 +0200

    Add basic stylings

--- src/Components/PostCard.js
+++ src/Components/PostCard.js
@@ -1,29 +1,16 @@
-import styled from 'styled-components';
 import data from '../Services/static-posts.json';
+import { CardAdresse, CardArticle, CardItem } from './StyledPostCard';
 
 export default function Posts() {
 	return (
-		<CardGrid>
+		<CardArticle>
 			<CardItem>
 				<h3>{data[0].name}</h3>
 				<p>{data[0].content}</p>
 				<h5>{data[0].contact}</h5>
-				<p>{data[0].mail}</p>
-				<p>{data[0].mobile}</p>
+				<CardAdresse>{data[0].mail}</CardAdresse>
+				<CardAdresse>{data[0].mobile}</CardAdresse>
 			</CardItem>
-		</CardGrid>
+		</CardArticle>
 	);
 }
-export const CardGrid = styled.div`
-flex-direction: row;
-flex-wrap;
-gap: 15px;
-padding: 10px;
-`;
-
-export const CardItem = styled.div`
-	list-style: none;
-	padding: 10px;
-	border-radius: 5px;
-	border-style: solid;
-`;
---
+++ src/Components/StyledPostCard.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const CardArticle = styled.article`
+	gap: 15px;
+	padding: 10px;
+`;
+
+export const CardItem = styled.div`
+	list-style: none;
+	padding: 10px;
+	border-radius: 5px;
+	box-shadow: 2px 2px 8px grey;
+`;
+export const CardAdresse = styled.p`
+	font-size: 0.8rem;
+`;
