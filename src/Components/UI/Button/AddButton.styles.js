8f95958a486a9e403f41c1c05551ea4accb6880b
Franzi Graml <franzi.graml@gmx.de>
2022-05-23 22:59:36 +0200

    Add basic styling Add button

---
+++ src/Components/UI/Button/AddButton.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const ButtonAdd = styled.button`
+    text-transform: none;
+	border-radius: 50%;
+	border: none;
+	background-color: lavender;
+	display: flex;
+	padding: 5px 5px;
+	cursor: pointer:
+	
+`;
+
+export default ButtonAdd;
