9cfc949f048de33d55e84503bfa0e8549df917ac
Franzi Graml <franzi.graml@gmx.de>
2022-05-27 10:20:09 +0200

    Add changes PR

--- src/Components/Form/AddFormButton.js
+++
@@ -1,9 +0,0 @@
-import ButtonAdd from '../UI/Form/Button/AddButton.styles';
-
-export default function AddFormButton({ onSetIsFormActive }) {
-	return (
-		<ButtonAdd type="button" onClick={() => onSetIsFormActive(prevCheck => !prevCheck)}>
-			Add new Post
-		</ButtonAdd>
-	);
-}
