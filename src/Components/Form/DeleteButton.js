e1d9a38d95eff33097791a3fbfe6bf21390aa517
Franzi Graml <franzi.graml@gmx.de>
2022-05-29 11:45:22 +0200

    Add delete comment

--- src/Components/Form/DeleteButton.js
+++ src/Components/Form/DeleteButton.js
@@ -2,11 +2,7 @@ import ButtonDelete from '../UI/Form/Button/DeleteButton.styles';
 
 export default function DeleteButton({ id, onSetPosts, posts }) {
 	function deletePost() {
-		onSetPosts(
-			posts.filter(post => {
-				return post.id !== id && post;
-			})
-		);
+		onSetPosts(posts.filter(post => post.id !== id));
 	}
 
 	return (
