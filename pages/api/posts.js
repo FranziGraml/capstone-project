8ca67c34a0abfecad2489b01e0630b154abf5ce3
Franzi Graml <franzi.graml@gmx.de>
2022-05-31 10:14:29 +0200

    Add delete console.log

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,7 +2,6 @@ import { getPosts } from '../../src/Services/get-posts';
 
 export default async function handler(req, res) {
 	const posts = await getPosts();
-	console.log('/api/posts: ' + posts);
 
 	res.status(200).json(posts);
 }
