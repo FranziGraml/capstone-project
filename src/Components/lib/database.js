4e7fc71cff0600d4677dc409936a12c157f7501c
Franzi Graml <franzi.graml@gmx.de>
2022-05-31 22:27:17 +0200

    implemented suggested changes from PR

--- src/Components/lib/database.js
+++ src/Components/lib/database.js
@@ -1,13 +1,10 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '../../../.env.local' });
 
 const url = process.env.DB_CONNECTION; /* 'mongodb://localhost:27017/digi-nomads'; */
-console.log(url);
+
 export async function dbConnect() {
 	try {
-		await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+		await mongoose.connect(url);
 		console.log('Connected to MongoDB');
 	} catch (error) {
 		console.error('ERROR could not connect', error.message);
