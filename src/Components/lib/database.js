15d02e5826675513e5de52c98cbed24716520bf4
Franzi Graml <franzi.graml@gmx.de>
2022-05-31 13:08:43 +0200

    add .env as secret to run-tests.yml

--- src/Components/lib/database.js
+++ src/Components/lib/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '../../../.env.local' });
 
-const url = 'mongodb://localhost:27017/digi-nomads'; // process.env.DB_CONNECTION ||
+const url = process.env.DB_CONNECTION; /* 'mongodb://localhost:27017/digi-nomads'; */
 console.log(url);
 export async function dbConnect() {
 	try {
