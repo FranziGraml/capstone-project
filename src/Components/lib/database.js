f18b45c646406747d07e59f92ea5e12cad555b32
Franzi Graml <franzi.graml@gmx.de>
2022-05-31 11:49:52 +0200

    added dotenv.config to database

--- src/Components/lib/database.js
+++ src/Components/lib/database.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const url = process.env.DB_CONNECTION; /* 'mongodb://localhost:27017/digi-nomads'; */
 console.log(url);
 export async function dbConnect() {
 	try {
-		await mongoose.connect(url);
+		await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 		console.log('Connected to MongoDB');
 	} catch (error) {
 		console.error('ERROR could not connect', error.message);
