f6353762f0b44c502e470cba4e770d8b6257f498
Franzi Graml <franzi.graml@gmx.de>
2022-05-31 12:32:36 +0200

    add alternative path to database

--- src/Components/lib/database.js
+++ src/Components/lib/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '../../../.env.local' });
 
-const url = process.env.DB_CONNECTION || 'test'; /* 'mongodb://localhost:27017/digi-nomads'; */
+const url = process.env.DB_CONNECTION || 'mongodb://localhost:27017/digi-nomads';
 console.log(url);
 export async function dbConnect() {
 	try {
